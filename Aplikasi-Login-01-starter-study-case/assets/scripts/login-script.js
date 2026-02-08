2315840b0f8c7a15ee997c3cf3d4b7d597656e5d
Annisa Latifa Alquraini <annisaalquraini1203@gmail.com>
2026-02-08 18:52:22 +0800

    Update login-script.js

--- Aplikasi-Login-01-starter-study-case/assets/scripts/login-script.js
+++ Aplikasi-Login-01-starter-study-case/assets/scripts/login-script.js
@@ -1,10 +1,3 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
